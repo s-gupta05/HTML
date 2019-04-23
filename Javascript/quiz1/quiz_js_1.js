87ac14a8cf6cac74a92f32cc1e7d8833ec939117
s-gupta05 <48992606+s-gupta05@users.noreply.github.com>
2019-04-23 18:20:28 +0530

    Add files via upload

---
+++ Javascript/quiz1/quiz_js_1.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var userAge = prompt("What is your Age?");
+var fullName = firstName + " " + lastName;
+console.log("Hello! Nice to meet you " + fullName);
+console.log("You are " + userAge + " years old");
