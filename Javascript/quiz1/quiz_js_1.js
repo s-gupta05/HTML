c5a03f82e47d457c4a966f92c81c51e14847f5a1
Sneha <ms.snehagupta2009@gmail.com>
2019-08-09 18:26:43 +0530

    deleted javascript folder

--- Javascript/quiz1/quiz_js_1.js
+++
@@ -1,6 +0,0 @@
-var firstName = prompt("What is your First Name?");
-var lastName = prompt("What is your Last Name?");
-var userAge = prompt("What is your Age?");
-var fullName = firstName + " " + lastName;
-console.log("Hello! Nice to meet you " + fullName);
-console.log("You are " + userAge + " years old");
--- Javascript/quiz2/quiz_js_2.js
+++
@@ -1,3 +0,0 @@
-var userAge = prompt("What is your Age?");
-var ageInDays = userAge * 365;
-console.log("You are roughly " + ageInDays + " days old!");
